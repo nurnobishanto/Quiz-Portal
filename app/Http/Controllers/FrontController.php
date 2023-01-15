4eb855de23ccf2e0154fd27db042d41b04c9a90c
Md Nurnobi Hosen <47949519+nurnobishanto@users.noreply.github.com>
2023-01-15 19:49:00 +0600

    update

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -13,7 +13,7 @@ class FrontController extends Controller
 {
     //
     public function index(){
-        return view('welcome');
+        return "Checking...";
         SEOTools::setTitle(setting('site.title')." - ".setting('site.tagline'));
         SEOTools::setDescription(setting('site.description'));
        // notify()->success('Welcome to '.setting('site.title').'⚡️');
